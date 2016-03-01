ff5519de780c07fd0c3aea7e72b0c277f4fb5597
ricejustin31 <rice.justin31@gmail.com>
2016-03-01 03:05:15 +0000

    Nav bar team picker now directs to team page.

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import os
+from flask.ext.socketio import SocketIO, emit
+import os, uuid
 import psycopg2
 import psycopg2.extras
 import sys
 reload(sys)
 sys.setdefaultencoding("UTF8")
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.secret_key = os.urandom(24).encode('hex')
+
+socketio = SocketIO(app)
 
 
 def connectToDB():
@@ -17,14 +20,26 @@ def connectToDB():
     return psycopg2.connect(connectionString)
   except:
     print("Can't connect to database")
-    
-
+  
 @app.route('/')
 def mainIndex():
+#    return render_template('index.html')
+    print('Im here.');
     return render_template('index.html')
     
+@app.route('/team')
+def new_view():
+    return render_template('team.html')
+        
+@socketio.on('connect', namespace='/points')
+def makeConnection(): 
+    print('connected')   
     
+@socketio.on('viewTeam', namespace='/points')
+def viewTeam(teamName):
+    emit('redirect', {'url': '/team'})
+ 
 
+# start the server
 if __name__ == '__main__':
-    app.debug=True
-    app.run(host='0.0.0.0', port=8080)
+        socketio.run(app, host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
--- static/js/controller.js
+++ static/js/controller.js
@@ -1,185 +1,20 @@
-var AwesomeChatApp = angular.module('AwesomeChat',[]);
-AwesomeChatApp.controller('ChatController', function($scope){
+var PowerPointsApp = angular.module('PowerPointsTracker',[]);
+PowerPointsApp.controller('PointsController', function($scope){
     
-    var socket=io.connect('https://'+document.domain+':'+location.port+'/awesome');
+    var socket=io.connect('https://'+document.domain+':'+location.port+'/points');
     
-    $scope.messages=[];
-    $scope.name='';
-    $scope.text='';
-    $scope.results=[];
-    $scope.users=[];
-    $scope.rooms=[];
-    $scope.searchRes=[];
-    $scope.failMessage="";
-    
-    socket.on('message', function(msg){
-       //console.log(msg);
-       $scope.messages.push(msg);
-       $scope.$apply();
-       var elem = document.getElementById('msgpane');
-       elem.scrollTop = elem.scrollHeight;
+    socket.on('redirect', function (newPage){
+        console.log("made it");
+        console.log(newPage);
+        window.location = newPage.url;
     });
     
-    socket.on('addUser', function(newUser){
-        $scope.users.push(newUser);
-        $scope.$apply();
-        
-    });
     
-    socket.on('loggedIn', function() {
-       var elem = document.getElementById('login');
-       elem.style.visibility = 'hidden'; 
-       var elem2 = document.getElementById('fail');
-       elem2.style.visibility = 'hidden'; 
-       $scope.loggedIn= 'true';
-       $scope.$apply();
-       
-    });
-    
-     socket.on('loggedOut', function() {
-       //console.log("Logging out ...");
-       $scope.loggedIn= 'false';
-       $scope.$apply();
-       //console.log($scope.loggedIn);
-    });
-
-    socket.on('logInFail', function() {
-       var elem = document.getElementById('fail');
-       elem.style.visibility = 'visible'; 
-       var elem2 = document.getElementById('login');
-       elem2.style.visibility = 'hidden'; 
-    });
-    
-    socket.on('users', function(user){
-        $scope.users.push(user);
-        $scope.$apply();
-    });
-    
-    socket.on('rooms', function(room){
-        $scope.rooms.push(room);
-        //console.log(room);
-        $scope.$apply();
-    });
-    
-    socket.on('removeuser', function(removedUser){
-        var counter = 0;
-        var index = 0;
-        var tempArray = [];
-        for (counter = 0; counter < $scope.users.length; counter ++){
-            if ($scope.users[counter] != removedUser){
-                tempArray.push($scope.users[counter]);
-            }
-        }
-        $scope.users = tempArray;
-        $scope.$apply();
-    });
-    
-    socket.on('searchRes', function(searchResults){
-        
-        $scope.searchRes.push(searchResults);
-        $scope.$apply();
-        var elem=document.getElementById('searchRes');
-        elem.style.visibility = 'visible'; 
-    });
-    
-    socket.on('newUserFail', function(msg){
-        var elem=document.getElementById('failCreate');
-        $scope.failMessage=msg;
-        elem.style.visibility = 'visible'; 
-        $scope.$apply();
-    });
-    
-    socket.on('madeNewUser', function(){
-        var elem=document.getElementById('failCreate');
-        $scope.failMessage='Account created!';
-        elem.style.visibility = 'visible'; 
-        $scope.$apply();
-    });
-    
-    $scope.login = function login(){
-       // console.log("Made it.");
-       socket.emit('login');
-       var elem = document.getElementById('login');
-       elem.style.visibility = 'visible'; 
-       var elem2 = document.getElementById('fail');
-       elem2.style.visibility = 'hidden'; 
-       //$scope.$apply();
+    $scope.teams = function teams(){
+      var teampick = document.getElementById("pickteam");
+      var tname = teampick.options[teampick.selectedIndex].value;
+      socket.emit('viewTeam', tname);
     };
     
-    $scope.logout = function logout(){
-       //console.log("Logging out");  
-       socket.emit('logout');
-       $scope.loggedIn='false';
-       //$scope.$apply();
-    };
-    
-    $scope.submitLog = function logUser(){
-      //console.log('trying to log in userName: ', $scope.username, ' with password ', $scope.password);
-      socket.emit('logUser', $scope.username, $scope.password);  
-    };
-    
-    $scope.send = function send(){
-     //   console.log('Sending message: ', $scope.text);
-        socket.emit('message', $scope.text);
-        $scope.text = '';
-    };
-    
-    $scope.changeR = function changeR(){
-        //console.log("In here.");
-        var roomName = document.forms["newRoomName"].elements["roomChange"].value;
-        socket.emit('changeRoom', roomName);
-        $scope.searchRes=[];
-        var elem=document.getElementById('searchRes');
-        elem.style.visibility = 'hidden'; 
-    };
-    
-    $scope.sear = function sear(){
-        //console.log("In search.");
-        $scope.searchRes=[];
-        var searchQ = document.forms["search"].elements["searchQ"].value;
-        socket.emit('search', searchQ);
-    };
-    
-    $scope.newUser = function newUser(){
-        //console.log("In create.");
-        $scope.failMessage="";
-        var pw1 = document.forms["create"].elements["firstPW"].value;
-        var pw2 = document.forms["create"].elements["secondPW"].value;
-        var newName = document.forms["create"].elements["newUserName"].value;
-        var elem=document.getElementById('failCreate');
-        if (newName.length < 1){
-            elem.style.visibility = 'visible'; 
-            $scope.failMessage="Must enter a user name!";
-        }
-        else if ((pw1 < 1) || (pw2 < 1)){
-            elem.style.visibility = 'visible'; 
-            $scope.failMessage="Must enter both password fields!";
-        }
-        else if (pw1 != pw2){
-            var elem=document.getElementById('failCreate');
-            elem.style.visibility = 'visible'; 
-            $scope.failMessage="Password's don't match";
-        }
-        else{
-            var theseRooms = [];
-            if (document.getElementById("awesomeCheck").checked){
-                theseRooms.push("awesomepeople");
-            }
-            if (document.getElementById("coolCheck").checked){
-                theseRooms.push("coolpeople");
-            }
-            if (document.getElementById("regularCheck").checked){
-                theseRooms.push("regularpeople");
-            }
-            //console.log(newName, pw1, theseRooms);
-            socket.emit('newUser', newName, pw1, theseRooms);
-        }
-    };
     
 });
-
-function setNewRoom(room){
-      document.forms["newRoomName"].elements["roomChange"].value=room;
-      //console.log('Value is', room);
-      
-    };
